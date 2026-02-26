ceb4edaf29d46708f19ac62e478b2c5b4194e3ad
Hevanafa <dhevansafa4@gmail.com>
2026-02-26 12:24:07 +0700

    added fetch thread prototype

--- unit1.pas
+++ unit1.pas
@@ -12,27 +12,46 @@ uses
   UAppContext, UAddURL;
 
 type
+  TSimpleCallback = procedure of object;
+
+  { TFetchThread }
+
+  TFetchThread = class(TThread)
+  private
+    fCallback: TSimpleCallback;
+    fHTMLData: string;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(callback: TSimpleCallback);
+    property HTMLData: string read fHTMLData;
+  end;
 
   { TForm1 }
 
   TForm1 = class(TForm)
     Button1: TButton;
+    HTMLMemo: TMemo;
     URLTesterLabel: TLabel;
     TestLabel: TLabel;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
+    fFetchThread: TFetchThread;
 
+    procedure StartFetchThread;
+    procedure LoadHTMLMemo;
   public
 
   end;
+
 
 var
   Form1: TForm1;
 
 implementation
 
-uses URIParser;
+uses URIParser, fphttpclient, opensslsockets;
 
 {$R *.lfm}
 
@@ -43,10 +62,22 @@ begin
 
 end;
 
+procedure TForm1.StartFetchThread;
+begin
+  HTMLMemo.text := 'Attempting to fetch ' + getURL;
+  fFetchThread := TFetchThread.create(@LoadHTMLMemo);
+end;
+
+procedure TForm1.LoadHTMLMemo;
+begin
+  HTMLMemo.Text := fFetchThread.HTMLData;
+end;
+
 procedure TForm1.Button1Click(Sender: TObject);
 var
   fAddUrl: TForm2;
   uri: TUri;
+  isValid: boolean;
 begin
   try
     fAddUrl := TForm2.Create(Self);
@@ -58,11 +89,42 @@ begin
   end;
 
   uri := ParseURI(getURL);
-  if (uri.protocol = 'https') and (uri.host <> '') then
+  isValid := (uri.protocol = 'https') and (uri.host <> '');
+  if isValid then
     URLTesterLabel.Caption := 'Valid URL'
   else
     URLTesterLabel.Caption := 'Not a valid URL!';
 
+  startFetchThread
+end;
+
+{ TFetchThread }
+
+procedure TFetchThread.Execute;
+var
+  client: TFPHTTPClient;
+  response: string;
+begin
+  try
+    client := TFPHTTPClient.Create(nil);
+    response := client.get(getURL);
+
+    fHTMLData := response;
+
+    try
+      Synchronize(fCallback)
+    finally
+    end;
+  finally
+    client.free;
+  end;
+end;
+
+constructor TFetchThread.Create(callback: TSimpleCallback);
+begin
+  inherited create(false);
+  FreeOnTerminate := true;
+  fCallback := callback;
 end;
 
 end.
